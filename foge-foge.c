6024819d3ae8615b8ee531c257957ffce10464e8
brunocnog <brunocesar.br@gmail.com>
2023-02-02 20:16:55 -0300

    Bloqueio de tecla
    Bloqueando teclas diferentes de a s w d;

--- foge-foge.c
+++ foge-foge.c
@@ -37,6 +37,13 @@ int acabou(){
 
 void move(char direcao){
 
+    if(
+        direcao !='a'&&
+        direcao !='d'&&
+        direcao !='s'&&
+        direcao !='w'
+    ) return;
+
     m.matriz[heroi.x][heroi.y] = '.';
 
     switch (direcao){
